8c75382878c517120f454e5aec2cf3a663d35f33
Taylor Otwell <taylor@laravel.com>
2026-07-09 10:27:02 -0500

    add doc blocks

--- src/Console/Commands/SkeletonCommand.php
+++ src/Console/Commands/SkeletonCommand.php
@@ -8,10 +8,19 @@ use Illuminate\Console\Command;
 
 class SkeletonCommand extends Command
 {
+    /**
+     * The command signature.
+     */
     protected $signature = 'skeleton:placeholder';
 
+    /**
+     * The command description.
+     */
     protected $description = 'Placeholder Artisan command shipped by the package skeleton.';
 
+    /**
+     * Execute the console command.
+     */
     public function handle(): int
     {
         $this->line('Skeleton placeholder command executed.');
